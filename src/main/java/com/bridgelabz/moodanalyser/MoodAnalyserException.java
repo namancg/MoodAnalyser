87b3068fff3435371357902f929c07c4b4e64318
namancg <naman.c@perfios.com>
2021-09-10 22:59:19 +0530

    ADDED: custom exception for mood analysis

---
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyserException.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.moodanalyser;
+
+public class MoodAnalyserException extends Exception{
+	enum ExceptionType{
+		ENTERED_NULL,ENTERED_EMPTY
+	}
+	ExceptionType type;
+	public MoodAnalyserException(ExceptionType type,String message) {
+		super(message);
+		this.type=type;
+	}
+}
