71b9c93a26d481f352edf20b52e56ff4038d222c
namancg <naman.c@perfios.com>
2021-09-10 23:01:31 +0530

    MODIFIED: try catch block added to catch thrown exception

--- src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -2,6 +2,9 @@ package com.bridgelabz.moodanalyser;
 import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import com.bridgelabz.moodanalyser.MoodAnalyserException.ExceptionType;
 
 public class MoodAnalyserTest {
 	@Test
@@ -20,13 +23,37 @@ public class MoodAnalyserTest {
 		Assert.assertThat(mood, CoreMatchers.is("MOOD IS HAPPY"));
 	}
 	@Test
-	public void givenMessage_WhenNull_ShouldReturnHappy() {
+	public void givenMessage_WhenNull_ShouldReturnHappy() throws MoodAnalyserException {
 		
 		MoodAnalyser moodAnalyzer = new MoodAnalyser();
 		String mood = moodAnalyzer.analyzeMood("");
 		Assert.assertEquals("HAPPY",mood);
 
 		
+	}
+	@Test
+	public void givenMessage_NoMessage_shouldThrowException() throws MoodAnalyserException {
+		MoodAnalyser moodAnalyser = new MoodAnalyser();
+		try {
+			String mood = moodAnalyser.analyzeMood("");
+			Assert.assertThat(mood, CoreMatchers.is("MOOD IS SAD"));
+		} catch (MoodAnalyserException e) {
+			Assert.assertEquals(ExceptionType.ENTERED_EMPTY, e.type);
+		}
+	}
+
+	@Test
+	public void givenmessage_NullMood_shouldReturnHappy() throws Exception {
+		MoodAnalyser moodAnalyser = new MoodAnalyser();
+		try {
+			String mood = moodAnalyser.analyzeMood(null);
+			ExpectedException exceptionRule = ExpectedException.none();
+			exceptionRule.expect(MoodAnalyserException.class);
+			Assert.assertThat(mood, CoreMatchers.is("MOOD IS HAPPY"));
+		} catch (MoodAnalyserException e) {
+			Assert.assertEquals(ExceptionType.ENTERED_NULL, e.type);
+		}
+
 	}
 	
 }
